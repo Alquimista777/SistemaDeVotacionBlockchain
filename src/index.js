6ae6f2980ed45b6e6ab5bb753b9a5a26aede29e2
santiagoCeballos <santiago.bike95@gmail.com>
2020-08-12 20:50:45 -0500

    sistema de votacion por blockchain

---
+++ src/index.js
@@ -0,0 +1,55 @@
+
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+
+import voting_artifacts from '../../build/contracts/Votacion.json'
+
+var Voting = contract(voting_artifacts);
+
+let candidatos = {"1": "1", "2": "2", "3":"3"}
+
+// funcion para votar a un candidato que se pasa como parametro. Una vez minada la transaccion se actualiza el contador
+window.votar = function(candidato) {
+  let nombreCandidato = $("#candidato").val();
+  try {
+    $("#msg").html("Tu voto ha sido emitido. El número de votos se actualizará cuando la transacción sea minada. Espera.")
+    $("#candidato").val("");
+
+    Voting.deployed().then(function(contractInstance) {
+      contractInstance.votar(nombreCandidato, {gas: 140000, from: "0x6A2c81f8874132702d578B5f97B9E1b8Cb133261"}).then(function() {
+        let div_id = candidatos[nombreCandidato];
+        return contractInstance.votosTotales.call(nombreCandidato).then(function(v) {
+          $("#" + div_id).html(v.toString());
+          $("#msg").html("");
+        });
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// cuando se carga la página se inicializa la conexión con la blockchain y se actualizan los votos de cada candidato
+$( document ).ready(function() {
+  if (typeof web3 !== 'undefined') {
+    console.warn("Usando web3 de fuente externa como Metamask")
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.warn("No web3 detectado. Redirifiendo a http://localhost:7545.");
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+  }
+
+  Voting.setProvider(web3.currentProvider);
+  let nombreCandidatos = Object.keys(candidatos);
+
+  for (var i = 0; i < nombreCandidatos.length; i++) {
+    let nombre = nombreCandidatos[i];
+    Voting.deployed().then(function(contractInstance) {
+      contractInstance.votosTotales.call(nombre).then(function(v) {
+
+        $("#" + candidatos[nombre]).html(v.toString());
+      });
+    })
+  }
+});
